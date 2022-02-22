692878869345783e30f5b4a1b50c3c8936cd57b6
Anisha McDowell <anishamcdowell@gmail.com>
2022-02-21 22:31:57 -0500

    Basic server made

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use((req, res) => {
+  res.send('Blueprint for Justice');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
